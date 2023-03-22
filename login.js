23b89cfc8bd70b359dc573c50f6603ed6d7941c2
PabloSouza27 <128435338+PabloSouza27@users.noreply.github.com>
2023-03-22 19:53:38 -0300

    Update login.js

--- login.js
+++ login.js
@@ -355,7 +355,7 @@ if (nome2 != undefined) {
             }
         }
         if (erro == 0) {
-            (alert("Login realizado com sucesso!")
+            (alert("Login realizado com sucesso!"))
             testaruserpass1()
         }
 
